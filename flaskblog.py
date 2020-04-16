b733ba51788cbb41cf16b8de4a55503a672fa912
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-04-16 21:33:43 +0530

    added posts data for testing

--- flaskblog.py
+++ flaskblog.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Amit Ghosh',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 21,2019'
+    },
+    {
+        'author': 'Sumit Ghosh',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 22,2019'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
